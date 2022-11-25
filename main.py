7eecc5a213e0eb1d724de378738846c779c3587c
Benji1995 <37215756+Benji1995@users.noreply.github.com>
2022-11-25 10:44:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def execute():
                                         )
     dhandler.handleData()
     meas_df = dhandler.convertMeasToDataframe()
+    print(meas_df)
 
     # print(meas_df)
     # dplot2DIon = dataplotter.Plotter2DIon(  df=meas_df,
@@ -24,24 +25,30 @@ def execute():
     #                                         plot_title="COP in functie van Ambient Temperature",
     #                                         ).plotIon()
                                             
-    plot(meas_df, xy_label=["Tambient", "COP"], setup_dict=setup_dict["Data"], plot_title="COP w.r.t. Tambient")
+    # plot(meas_df, xy_label=["Tambient", "COP"], setup_dict=setup_dict["Data"])
 
     
-def plot(meas_df, xy_label, setup_dict, plot_title):
-    # fig, ax = plt.subplot
-    rng = [1000,1100]
-    df_part = meas_df.loc[meas_df["averageCapacity"].between(*rng)]
-    x,y = list(df_part['averageAmbient']), list(df_part["averageSample"])
+def plot(meas_df, xy_label, setup_dict, plot_title=""):
+    
+    # rng = [1000,1100]
+    # df_part = meas_df.loc[meas_df["averageCapacity"].between(*rng)]
+    # x,y = list(df_part['averageAmbient']), list(df_part["averageSample"])
 
-    plt.plot(x,y, ".", label = "Q in range {}-{} W".format(rng[0], rng[1]))
-    plt.xlabel(xlabel=xy_label[0])
-    plt.ylabel(ylabel=xy_label[1])
-    plt.xlim(setup_dict["AmbientRange"][0],setup_dict["AmbientRange"][1])
-    plt.ylim(setup_dict["COPRange"][0], setup_dict["COPRange"][1])
-    plt.title = plot_title
-    # plt.legend()
-    plt.grid()
-    plt.show()
+    # fig, axs = plt.subplots(1,1, figsize=(10,5))
+
+    # axs[0] = 
+
+
+    # plt.scatter(x,y)
+    # plt.title = "COP w.r.t. Tambient for Q in range {}-{} W".format(rng[0], rng[1])
+    # plt.xlabel(xlabel=xy_label[0])
+    # plt.ylabel(ylabel=xy_label[1])
+    # plt.xlim(setup_dict["AmbientRange"][0],setup_dict["AmbientRange"][1])
+    # plt.ylim(setup_dict["COPRange"][0], setup_dict["COPRange"][1])
+    # # plt.legend()
+    # plt.grid()
+    
+    # plt.show()
     pass    
 
 if __name__ == "__main__":
